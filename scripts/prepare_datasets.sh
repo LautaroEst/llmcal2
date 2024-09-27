a5489a7ff6813e8630b4e7883f48ca5f6747dba6
LautaroEst <lautaro.est@gmail.com>
2024-09-27 11:53:21 -0300

    add train test lists

--- scripts/prepare_datasets.sh
+++ scripts/prepare_datasets.sh
@@ -5,5 +5,5 @@ DATASETS="sst2 agnews dbpedia 20newsgroups banking77"
 
 for dataset in $DATASETS; do
     mkdir -p $OUTPUT_DIR/$dataset
-    python -m llmcal2.scripts.prepare_datasets --dataset $dataset --output_dir $OUTPUT_DIR/$dataset
+    python -m llmcal2.scripts.prepare_dataset --dataset $dataset --lists_dir lists/$dataset --output_dir $OUTPUT_DIR/$dataset
 done
