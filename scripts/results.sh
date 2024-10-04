ba766b826ad1844a781b5b039b153961ecc36ed1
LautaroEst <lautaro.est@gmail.com>
2024-10-04 02:08:58 -0300

    minor fix in dbpedia list

--- scripts/results.sh
+++ scripts/results.sh
@@ -4,8 +4,8 @@ mkdir -p outputs/results
 python -m llmcal2.scripts.plot_methods_vs_size \
     --model tinyllama \
     --metrics ner,nce,cal_loss_nce \
-    --datasets "sst2,agnews,dbpedia,20newsgroups,banking77" \
-    --methods "lora,lora_norm,dp_calibration,temp_scaling,no_adaptation,lora_plus_dp_calibration_no_es,lora_norm_plus_dp_calibration_no_es" \
+    --datasets "sst2," \
+    --methods "lora,dp_calibration,temp_scaling,no_adaptation" \
     --results_dir outputs/adaptation \
     --output_dir outputs/results \
     --overwrite
